762fa1db366d19788175e5cfd8d0364c101e85d0
othmaneuw <strangeothmane@gmail.com>
2023-12-09 13:51:28 +0100

    many to many

--- src/main/java/com/formation/spring/data/jpa/tutorial/repository/CourseRepository.java
+++ src/main/java/com/formation/spring/data/jpa/tutorial/repository/CourseRepository.java
@@ -1,9 +1,13 @@
 package com.formation.spring.data.jpa.tutorial.repository;
 
 import com.formation.spring.data.jpa.tutorial.entity.Course;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
+    //Page<Course> findByTitleContaining(String title, Pageable pageable);
 }
--- src/test/java/com/formation/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ src/test/java/com/formation/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.formation.spring.data.jpa.tutorial.repository;
 
 import com.formation.spring.data.jpa.tutorial.entity.Course;
+import com.formation.spring.data.jpa.tutorial.entity.Student;
 import com.formation.spring.data.jpa.tutorial.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -17,24 +20,58 @@ class CourseRepositoryTest {
     private CourseRepository courseRepository;
 
     @Test
-    public void listAllCourses(){
+    public void listAllCourses() {
         List<Course> courses = courseRepository.findAll();
-        System.out.println("Courses = "+courses);
+        System.out.println("Courses = " + courses);
     }
 
     @Test
-    public void saveCourse(){
+    public void saveCourse() {
 
         Teacher teacher = Teacher.builder()
-                .firstName("taha")
+                .firstName("adam")
                 .lastName("khemmar")
                 .build();
         Course course = Course.builder()
-                .title("Python")
+                .title("JEE")
                 .credit(7)
                 .teacher(teacher)
                 .build();
 
         courseRepository.save(course);
     }
+
+    /*@Test
+    public void findAllPagination(){
+        Pageable pageThreeRecords = PageRequest.of(0,3);
+       long courses = courseRepository.findAll(pageThreeRecords).getTotalElements();
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void printFindByTitleContaining(){
+        Pageable pageThreeRecords = PageRequest.of(0,2);
+        List<Course> courses = courseRepository.findByTitleContaining("A",(Pageable) pageThreeRecords).getContent();
+    }*/
+
+    @Test
+    public void saveCourseWithTeacherAndStudents(){
+        Teacher teacher = Teacher.builder()
+                .firstName("said")
+                .lastName("khemar")
+                .build();
+        Student student = Student.builder()
+                .firstName("kenza")
+                .lastName("mehlil")
+                .emailId("kenza@gmail.com")
+                .build();
+        Course course = Course.builder()
+                .title("JPA")
+                .credit(10)
+                .teacher(teacher)
+                .build();
+        course.addStudent(student);
+        courseRepository.save(course);
+    }
+
 }
